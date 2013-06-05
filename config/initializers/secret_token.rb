c72d2efd1f351ce6add260b91865976813311639
mohanraj.r <mohanraj.r@payoda.com>
2013-06-05 17:29:03 +0530

    Devise gem integration.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oliarakkattalai::Application.config.secret_token = '670a36ca0120a4aef67e0151e2065db9a7b8ef65d46c68336ab2791768e98f6cba6fc35a7d462702522c6bb8f4bfdeac099b1f5d1bed5f41e9251d2d256e30de'
